34d43152bf925a98836e0e0703e0af11e9e8020c
MauJSGreg <mauricio.silva@mevo.com.br>
2025-08-18 15:06:53 -0300

    Update receita.spec.ts

--- e2e/receita.spec.ts
+++ e2e/receita.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { describe } from 'node:test';
 
-const URL_BASE = 'https://teste-qa.d3vl799vf6z3om.amplifyapp.com/{{TOKEN}}';
-const URL_CARRINHO = 'https://teste-qa.d3vl799vf6z3om.amplifyapp.com/{{TOKEN}}/checkout';
+const URL_BASE = 'https://s.mevosaude.com.br/{{TOKEN}}';
+const URL_CARRINHO = 'https://s.mevosaude.com.br/{{TOKEN}}/checkout';
 // const RECEITA_TESTE = 'MIXF53M';
 
 describe('Receita', () => {
